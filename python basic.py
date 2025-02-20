5c7c79edf77758a977f169959d9d9384ae6d0b97
JoeyJackson4 <1jacksonjoe@hdsb.ca>
2025-02-20 14:24:46 -0500

    basics of python
    I copy the commands on the screen.

---
+++ python basic.py
@@ -0,0 +1,44 @@
+""""
+print(2+2)
+print("2")
+print("2+2")
+
+
+x = "2"
+y = 2
+print(int(x)+y)
+
+
+print("the number in variable y is "+str(y))
+
+
+
+print("these are bolinens")
+logged_in = True 
+
+print(logged_in)
+
+logged_out = False 
+
+print(logged_out)
+
+print(logged_in + logged_out)
+
+print(bool(logged_in + logged_out))
+
+
+
+
+"""
+
+print("Please enter your name: ")
+name = input()
+print("Your name is:" + name)
+
+name = input("Please enter your name: ")
+print("Your name is:" + name)
+
+age = int(input("please enter your age: "))
+#print ("please enter your age: ")
+#age = int(input())
+print(age)
