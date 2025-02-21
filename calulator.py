bf6b0390aa1c765e725961019621f70baabc8844
JoeyJackson4 <1jacksonjoe@hdsb.ca>
2025-02-21 14:36:30 -0500

    finished calulator
    added *, /, **.

--- calulator.py
+++ calulator.py
@@ -1,4 +1,4 @@
-operation = int(input("type 0 for +, type 1 for -: "))
+operation = int(input("type 0 for +, type 1 for -, type 2 for *, type 3 for /, type 4 for **: "))
 
 if(operation == 0):
     number1 = int(input("enter your first number: "))
@@ -10,7 +10,8 @@ if(operation == 0):
         print("The answer is:",number1+number2)
     if(confirm == 0):
         print("please restart")
-    
+    #this is for additon and getting the users input.
+
 if(operation == 1):
     number1 = int(input("enter your first number: "))
     print(number1)
@@ -21,4 +22,41 @@ if(operation == 1):
         print("The answer is:",number1-number2)
     if(confirm == 0):
         print("please restart")
-    
+        #this is for subtraction and getting the user input.
+
+if(operation == 2):
+    number1 = int(input("enter your first number: "))
+    print(number1)
+    number2 = int(input("enter your second number: "))
+    print(number2)
+    confirm = int(input("please confirm your answer type 1, if not type 0: "))
+    if(confirm == 1):
+        print("The answer is:",number1*number2)
+    if(confirm == 0):
+        print("please restart")
+        #this is for multiplication and getting the user input.
+
+if(operation == 3):
+    number1 = int(input("enter your first number: "))
+    print(number1)
+    number2 = int(input("enter your second number: "))
+    print(number2)
+    confirm = int(input("please confirm your answer type 1, if not type 0: "))
+    if(confirm == 1):
+        print("The answer is:",number1/number2)
+    if(confirm == 0):
+        print("please restart")
+        #this is for division and getting the user input.
+
+if(operation == 4):
+
+    number1 = int(input("enter your first number: "))
+    print(number1)
+    number2 = int(input("enter your second number: "))
+    print(number2)
+    confirm = int(input("please confirm your answer type 1, if not type 0: "))
+    if(confirm == 1):
+        print("The answer is:",number1**number2)
+    if(confirm == 0):
+        print("please restart")
+        #this is for power of and getting the user input.
